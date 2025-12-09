508de28c80074f6e784d1c86c6f98d75cf4395c9
jorinho <jorinho@jota.one>
2025-12-09 18:27:40 +0100

    initial settings for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+import vue from "@astrojs/vue";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          ssr: {
+            noExternal: ["vue", "pocketbase", "@primevue/themes", "@primeuix/themes"],
+          },
+        }
+      : {}),
+  },
+
+  integrations: [vue({ appEntrypoint: "/src/pages/_app" })],
+  outDir: "pb/pb_public",
+
+  ...(process.env.NODE_ENV === "production"
+    ? {
+        server: {
+          host: '127.0.0.1'
+        },
+      }
+    : {}),
+
+  adapter: node({
+    mode: "standalone",
+  }),
+});
---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export default {
+  apiBaseUrl: import.meta.env.PUBLIC_PB_BASE_URI,
+}
