9648f309808c76d46ee5dddc5dab04e4c4f24344
jorinho <jorinho@jota.one>
2025-12-15 10:41:40 +0100

    remove element-plus bootstrap (for now)

--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,9 +1,9 @@
-import { createApp } from 'vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+import { createApp } from 'vue'
+// import ElementPlus from "element-plus";
+// import "element-plus/dist/index.css";
 
 export default (App: any) => {
-  const app = createApp(App);
-  app.use(ElementPlus);
-  return app;
-};
+  const app = createApp(App)
+  // app.use(ElementPlus);
+  return app
+}
