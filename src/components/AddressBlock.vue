4137e62d06c63c0ec46d9b7216bb60e551f34676
jorinho <jorinho@jota.one>
2026-05-25 19:36:25 +0200

    add the contact page (and POC the unCMSizing of the website)

---
+++ src/components/AddressBlock.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container superslim">
+    <div v-if="person" class="address">
+      <h3 class="name">{{ person.firstName }} {{ person.lastName }}</h3>
+      <em v-if="person.title" class="title">{{ person.title }}</em>
+      <div class="street">{{ person.street }}</div>
+      <div class="city">{{ person.zip }} {{ person.city }}</div>
+      <div v-if="person.phone" class="phone">{{ person.phone }}</div>
+      <div class="email">
+        <a :href="`mailto:${person.email}`">{{ person.email }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Person {
+  firstName: string
+  lastName: string
+  title?: string
+  street: string
+  zip: number | string
+  city: string
+  phone?: string
+  email: string
+}
+
+defineProps<{ person?: Person }>()
+</script>
+
+<style lang="postcss" scoped>
+.address {
+  font-size: 1rem;
+}
+
+.name {
+  font-size: 1.4rem;
+  font-weight: 700;
+}
+
+.email {
+  margin-top: 1rem;
+  font-weight: 600;
+}
+</style>
